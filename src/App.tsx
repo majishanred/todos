1895adb8f94c8e07453223311902949e20e7158a
Matvey Kalashnikov <kmgpmain@gmail.com>
2024-03-17 23:18:55 +0200

    Add base layout for app background and effect for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+  useEffect(() => {
+    document.documentElement.classList.add(theme);
+
+    return () => document.documentElement.classList.remove(theme);
+  }, [theme]);
 
   return (
-    <></>
+    <>
+      <img className="fixed w-full h-auto -z-40" src={theme == 'dark' ? '/bg-desktop-dark.jpg' : '/bg-desktop-light.jpg'} />
+      <div className="fixed w-full h-full bg-[hsl(237,14%,26%)] -z-50"></div>
+      <div className="flex justify-center w-full text-white">
+        <div className="grid grid-cols-[minmax(540px,640px)] mt-10">
+          <Header theme={theme} setTheme={setTheme}></Header>
+        </div>
+      </div>
+    </>
   )
 }
 
